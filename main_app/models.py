7ba3ec319cfdb3cb30711bcdfc47dee5737e855f
Mohamed Ali Kadhem <mhejiy@gmail.com>
2025-09-15 11:26:20 +0300

    add image to watch model

--- main_app/models.py
+++ main_app/models.py
@@ -14,7 +14,7 @@ class Watch(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-
+    image = models.ImageField(upload_to='watch_images/', blank=True, null=True)
     def __str__(self):
         return f"{self.name} ({self.brand})"
     
