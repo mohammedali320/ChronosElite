d679ce46b83ac391c5697fab43e83747a003e0bb
Mohamed Ali Kadhem <mhejiy@gmail.com>
2025-09-16 09:48:43 +0300

    styling log in page

--- chronoselite/settings.py
+++ chronoselite/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/about'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- main_app/views.py
+++ main_app/views.py
@@ -8,9 +8,6 @@ class CustomLoginView(LoginView):
     template_name = 'login.html' 
 
 # Define the home view function
-def home(request):
-    # Send a simple HTML response
-    return HttpResponse('<h1>Hello</h1>')
 
 def about(request):
     return render(request, 'about.html')
