bf038efccffb0d08fb231bc2b295b2b146a1b9a0
Mohamed Ali Kadhem <mhejiy@gmail.com>
2025-09-16 09:13:51 +0300

    link category to a user

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=50)  # e.g., Mens, Womens, Kids
     description = models.TextField(blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
