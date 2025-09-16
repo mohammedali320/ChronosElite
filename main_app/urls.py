75ed6aaf3ed0bfaeacca57f336412bddbe044897
Mohamed Ali Kadhem <mhejiy@gmail.com>
2025-09-16 13:42:07 +0300

    styling cart,checkout and about

--- main_app/urls.py
+++ main_app/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path('cart/', views.view_cart, name='view_cart'),
     path('watch/<int:watch_id>/add_to_cart/', views.add_to_cart, name='add_to_cart'),
     path('checkout/', views.checkout, name='checkout'),
-
+    path('cart/remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
     
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -126,3 +126,9 @@ def checkout(request):
     cart.items.all().delete()
 
     return render(request, 'checkout.html', {'total': total})
+
+@login_required
+def remove_from_cart(request, item_id):
+    item = get_object_or_404(CartItem, id=item_id, cart__user=request.user)
+    item.delete()
+    return redirect('view_cart')
